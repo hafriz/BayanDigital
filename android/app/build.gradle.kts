f282b9fdd718f450899178fb886a0c94d2d9c0b5
Hafriz Azhan <hafriz@example.com>
2026-07-20 14:02:15 +0800

    Enable Android BuildConfig version constants

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
     namespace = "com.bayandigital.masjidscreen"
     compileSdk = 35
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
